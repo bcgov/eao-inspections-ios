eb85fe6c374ab5318c49e856f73c453473076a37
Micha Volin <Miha@main.hitronhub.home>
2017-03-15 09:29:17 -0700

    *Added "InspectionForm" template file *Added "DocumentPreview" template file

---
+++ EAO/DocumentPreviewController.swift
@@ -0,0 +1,15 @@
+//
+//  DocumentDetail.swift
+//  EAO
+//
+//  Created by Micha Volin on 2017-03-13.
+//  Copyright © 2017 FreshWorks. All rights reserved.
+//
+
+import QuickLook
+
+class DocumentPreviewController: QLPreviewController{
+    
+    
+    
+}
---
+++ EAO/InspectionFormController.swift
@@ -0,0 +1,9 @@
+//
+//  InspectionFormController.swift
+//  EAO
+//
+//  Created by Micha Volin on 2017-03-15.
+//  Copyright © 2017 FreshWorks. All rights reserved.
+//
+
+import Foundation
