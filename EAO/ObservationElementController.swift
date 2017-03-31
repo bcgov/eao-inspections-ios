2b070c4af106535b27e79692171a2f2b71714e8c
Micha Volin <Miha@main.hitronhub.home>
2017-03-30 17:16:47 -0700

    *Completed MVP requirements

--- EAO/ObservationElementController.swift
+++ EAO/ObservationElementController.swift
@@ -8,10 +8,37 @@
 
 class ObservationElementController: UIViewController{
 	
-	@IBOutlet var stackView: UIStackView!
+	@IBOutlet var scrollView : UIScrollView!
+	@IBOutlet var stackView  : UIStackView!
 	
+	var saveAction: (()->Void)?
+	
+	var counter = 0{
+		didSet{
+			if counter > 4{
+				counter = 4
+			}
+		}
+	}
+	
+	@IBAction func saveTapped(_ sender: UIBarButtonItem) {
+		saveAction?()
+		_ = navigationController?.popViewController(animated: true)
+	}
+ 
 	@IBAction func addPhotoTapped(_ sender: UIButton) {
+		let uploadPhotoController = UploadPhotoController.storyboardInstance() as! UploadPhotoController
 		
+		uploadPhotoController.uploadPhotoAction = { (image) in
+			(self.stackView.subviews[self.counter] as? UIImageView)?.image = image
+			self.counter += 1
+			
+		}
+		push(controller: uploadPhotoController)
+	}
+	
+	override func viewDidLoad() {
+		addDismissKeyboardOnTapRecognizer(on: scrollView)
 	}
 	
 }
---
+++ EAO/SettingsController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsController.swift
+//  EAO
+//
+//  Created by Micha Volin on 2017-03-30.
+//  Copyright © 2017 FreshWorks. All rights reserved.
+//
+
+import Foundation
